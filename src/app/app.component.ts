6e23e829d9c2aec06e457f785266235ec12f4ab6
lisop <lisophorm@gmail.com>
2021-05-09 22:42:48 +0100

    First commit boilerplate and angular material

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'AlfonsoSample';
+
+  constructor() {
+    // @ts-ignore
+    console.log('hello');
+  }
+
+
+}
